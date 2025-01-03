f35711b8226ed23231f319af505d9cb49cc23bbb
Iamtrieves <indokdavid2020@gmail.com>
2025-01-03 17:40:25 +0100

    Initial commit

---
+++ src/Components/AboutPageHero.jsx
@@ -0,0 +1,11 @@
+import CurveHeroBackground from "../assets/Images/about-page-images/hero-section-curve.svg";
+
+const AboutPageHero = () => {
+  return (
+    <div className="pt-[5rem] bg-[url('../assets/Images/about-page-images/hero-section-curve.svg')]">
+      <h1>This is the About Page</h1>
+    </div>
+  );
+};
+
+export default AboutPageHero;
---
+++ src/Pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import AboutPageHero from "../Components/AboutPageHero";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <AboutPageHero />
+    </div>
+  );
+};
+
+export default AboutPage;
